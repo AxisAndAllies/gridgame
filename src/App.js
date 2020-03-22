fdbbd2ff725676b9096dc2a0264b9bb34e951308
cktang88 <arctangent20@yahoo.com>
2020-03-21 20:30:27 -0500

    r

--- src/App.js
+++ src/App.js
@@ -13,13 +13,68 @@ function IsDraw(cells) {
   return cells.filter(c => c === null).length === 0;
 }
 
+function getOwnedBy(cells, owner) {
+  return cells.filter(c => c.owner === owner);
+}
+
+function getOccupied(cells) {
+  return cells.filter(c => c.owner);
+}
+
+function getSurrounding(cells, i, j) {
+  let id = BOARD_SIZE * i + j;
+  return [id - 1, id + 1, id - i, id + i];
+}
+
 const Main = {
   setup: () => ({ cells: Array(BOARD_SIZE * BOARD_SIZE).fill(null) }),
 
   moves: {
-    clickCell: (G, ctx, id) => {
+    attack: (G, ctx, id) => {
       if (G.cells[id] === null) {
-        G.cells[id] = ctx.currentPlayer;
+        G.cells[id] = {
+          owner: ctx.currentPlayer
+        };
+      }
+      G.endStage();
+    },
+    reinforce: (G, ctx, id) => {
+      if (G.cells[id] === null) {
+        G.cells[id] = {
+          owner: ctx.currentPlayer,
+          strength: 10
+        };
+      }
+      G.endStage();
+    }
+  },
+
+  turn: {
+    stages: {
+      reinforce: {
+        moves: {
+          reinforce: (G, ctx, id) => {
+            if (G.cells[id] === null) {
+              G.cells[id] = {
+                owner: ctx.currentPlayer
+              };
+            }
+            G.endStage();
+          }
+        },
+        next: "attack"
+      },
+      attack: {
+        moves: {
+          attack: (G, ctx, id) => {
+            if (G.cells[id] === null) {
+              G.cells[id] = {
+                owner: ctx.currentPlayer
+              };
+            }
+            G.endStage();
+          }
+        }
       }
     }
   },
