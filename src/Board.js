1d3ed1793840a2c4f637ec9ee577d18efc4cf066
cktang88 <arctangent20@yahoo.com>
2020-03-22 12:52:53 -0500

    r

--- src/Board.js
+++ src/Board.js
@@ -1,19 +1,22 @@
 import React from "react";
 
-export const BOARD_SIZE = 50;
+export const BOARD_SIZE = 100;
 
-let Cell = ({ id, owner, onClick }) => {
-  const CELL_SIZE = 20;
+let _cell = ({ id, cell, onClick }) => {
+  let owner = cell?.owner;
+  const CELL_SIZE = 30;
 
   const cellStyle = owner => ({
     width: `${CELL_SIZE}px`,
+    minWidth: `${CELL_SIZE}px`,
     height: `${CELL_SIZE}px`,
     lineHeight: `${CELL_SIZE}px`,
     maxWidth: `${CELL_SIZE}px`,
-    overflow: "hidden",
-    textAlign: "center",
+    left: `${CELL_SIZE * (id % BOARD_SIZE)}px`,
+    top: `${CELL_SIZE * Math.floor(id / BOARD_SIZE)}px`,
     background: owner == null ? null : owner == 1 ? "tomato" : "steelblue" // https://www.w3schools.com/colors/colors_names.asp
   });
+  // console.log(CELL_SIZE * (id % BOARD_SIZE), (CELL_SIZE * id) / BOARD_SIZE);
 
   return (
     <td
@@ -22,10 +25,17 @@ let Cell = ({ id, owner, onClick }) => {
       key={id}
       onClick={() => onClick(id)}
     >
-      {owner}
+      {cell?.strength}
     </td>
   );
 };
+
+let Cell = React.memo(
+  _cell,
+  (prevProps, nextProps) =>
+    prevProps.id == nextProps.id &&
+    JSON.stringify(prevProps.cell) == JSON.stringify(nextProps.cell)
+);
 
 class Board extends React.Component {
   onClick(id) {
@@ -44,6 +54,18 @@ class Board extends React.Component {
   cells() {
     return this.props.G.cells;
   }
+  componentDidUpdate(prevProps, prevState) {
+    Object.entries(this.props).forEach(
+      ([key, val]) =>
+        prevProps[key] !== val && console.log(`Prop '${key}' changed`)
+    );
+    if (this.state) {
+      Object.entries(this.state).forEach(
+        ([key, val]) =>
+          prevState[key] !== val && console.log(`State '${key}' changed`)
+      );
+    }
+  }
 
   render() {
     let winner = "";
@@ -56,31 +78,31 @@ class Board extends React.Component {
         );
     }
 
-    let tbody = Array(BOARD_SIZE)
-      .fill()
-      .map((_, i) => (
-        <tr key={i}>
-          {Array(BOARD_SIZE)
-            .fill()
-            .map((_, j) => {
-              const id = BOARD_SIZE * i + j;
-              const owner = this.props.G.cells[id]?.owner;
-              return (
-                <Cell
-                  key={id}
-                  owner={owner}
-                  id={id}
-                  onClick={this.onClick.bind(this)}
-                />
-              );
-            })}
-        </tr>
-      ));
-
     return (
-      <div>
+      <div id="container">
         <table id="board">
-          <tbody>{tbody}</tbody>
+          <tbody>
+            {Array(BOARD_SIZE)
+              .fill()
+              .map((_, i) => (
+                <tr key={i}>
+                  {Array(BOARD_SIZE)
+                    .fill()
+                    .map((_, j) => {
+                      const id = BOARD_SIZE * i + j;
+                      const cell = this.props.G.cells[id];
+                      return (
+                        <Cell
+                          key={id}
+                          cell={cell}
+                          id={id}
+                          onClick={this.onClick.bind(this)}
+                        />
+                      );
+                    })}
+                </tr>
+              ))}
+          </tbody>
         </table>
         {winner}
       </div>
